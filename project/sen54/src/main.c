b0c0fad20287415c3f868b8ba746ca801f110964
Oliver Roman <o.roman@uqconnect.edu.au>
2022-05-31 05:06:35 -0700

    SEN54 is integrated into oslib and has a snippet in main that works, figure out if you need to continuously poll (i.e. not perform one measurement per function call) and VOC needs checking to see if it correctly updates, and a general cleanup would be good

--- project/sen54/src/main.c
+++ project/sen54/src/main.c
@@ -8,7 +8,13 @@
 #include "scu_ble.h"
 #include "scu_power_management.h"
 #include "scu_sensors.h"
-
-
-
-
+#include "sen5x_i2c.h"
+#include "sensirion_common.h"
+#include "sensirion_i2c_hal.h"
+void main(void)
+{
+        while(1) {
+                scu_process_sen54_sample();
+                k_sleep(K_SECONDS(1));
+        }
+}
